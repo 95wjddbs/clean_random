8c18054311d9e5409bedefdd06ce82bba3a3a3ac
95wjddbs <95wjddbs@naver.com>
2021-08-18 15:33:10 +0900

    완성

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   ScrollView,
   StyleSheet,
@@ -12,32 +12,27 @@ import {
 
 import data from './dummydata.json';
 
-let arr = [
+function shuffle(array) {
+  array?.sort(() => Math.random() - 0.5);
+}
+const numbers = [
   '김유태',
-  '이창욱',
-  '심평섭',
   '박주안',
+  '이창욱',
   '허지행',
+  '심평섭',
   '박진',
   '김인철',
   '황아영',
   '이정윤',
   '마이크',
 ];
-const shuffle = array => {
-  for (let i = 0; i < array.length; i++) {
-    let j = Math.floor(Math.random() * (i + 1));
-    const x = array[i];
-    array[i] = array[j];
-    array[j] = x;
-  }
-  return array;
-};
+shuffle(numbers);
+console.log(numbers);
 
-shuffle(arr);
-
-console.log(111, arr);
 const App = () => {
+  const [status, setStatus] = useState(false);
+
   const renderRow = ({item, index}) => {
     return (
       <View
@@ -50,7 +45,7 @@ const App = () => {
           alignItems: 'center',
         }}>
         <Image
-          source={require('./image/recycle.png')}
+          source={{uri: item.img}}
           style={{
             width: '100%',
             height: 91,
@@ -60,12 +55,14 @@ const App = () => {
         />
         <Text style={{fontSize: 16, fontWeight: 'bold'}}>
           {item.clean_name + ' :'}
+          {status === true ? <Text>{numbers[index]}</Text> : null}
         </Text>
       </View>
     );
   };
   return (
     <>
+      <View style></View>
       <View
         style={{
           flexDirection: 'row',
@@ -81,24 +78,55 @@ const App = () => {
         <Text style={{fontSize: 24, fontWeight: 'bold'}}>청소 랜덤 뽑기</Text>
       </View>
 
-      <TouchableOpacity
+      <View
         style={{
-          width: Dimensions.get('window').width - 80,
-          marginLeft: 40,
+          flexDirection: 'row',
           alignItems: 'center',
-          justifyContent: 'center',
-          marginTop: 40,
-          height: 50,
-          borderRadius: 10,
-          backgroundColor: '#27A1FF',
-          marginBottom: 60,
+          justifyContent: 'space-evenly',
         }}>
-        <Text style={{color: '#fff', fontSize: 17, fontWeight: 'bold'}}>
-          랜덤 뽑기
-        </Text>
-      </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => shuffle(numbers)}
+          style={{
+            width: Dimensions.get('window').width / 2 - 40,
+            alignItems: 'center',
+            justifyContent: 'center',
+            marginTop: 40,
+            height: 50,
+            borderRadius: 10,
+            backgroundColor: '#27A1FF',
+            marginBottom: 60,
+          }}>
+          <Text style={{color: '#fff', fontSize: 17, fontWeight: 'bold'}}>
+            랜덤 셔플
+          </Text>
+        </TouchableOpacity>
 
-      <FlatList data={data.type} renderItem={renderRow} numColumns={2} />
+        <TouchableOpacity
+          onPress={() => setStatus(prev => !prev)}
+          style={{
+            width: Dimensions.get('window').width / 2 - 40,
+            alignItems: 'center',
+            justifyContent: 'center',
+            marginTop: 40,
+            height: 50,
+            borderRadius: 10,
+            backgroundColor: '#fff',
+            marginBottom: 60,
+            borderWidth: 2,
+            borderColor: '#27A1FF',
+          }}>
+          <Text style={{color: '#27A1FF', fontSize: 17, fontWeight: 'bold'}}>
+            결과 보기
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <FlatList
+        data={data.type}
+        renderItem={renderRow}
+        numColumns={2}
+        keyExtractor={data => data.clean_name.toString()}
+      />
     </>
   );
 };
