5b3bd1d191f016462f58c5c87932d1edb746d242
95wjddbs <95wjddbs@naver.com>
2021-08-18 16:07:05 +0900

    90%

--- App.js
+++ App.js
@@ -62,71 +62,72 @@ const App = () => {
   };
   return (
     <>
-      <View style></View>
-      <View
-        style={{
-          flexDirection: 'row',
-          alignItems: 'center',
-          justifyContent: 'center',
-          marginRight: 20,
-          marginTop: 20,
-        }}>
-        <Image
-          source={require('./image/1.png')}
-          style={{width: 100, height: 100, resizeMode: 'contain'}}
+      <ScrollView style={{backgroundColor: '#fff'}}>
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'center',
+            marginRight: 20,
+            marginTop: 20,
+          }}>
+          <Image
+            source={require('./image/1.png')}
+            style={{width: 100, height: 100, resizeMode: 'contain'}}
+          />
+          <Text style={{fontSize: 24, fontWeight: 'bold'}}>청소 랜덤 뽑기</Text>
+        </View>
+
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-evenly',
+          }}>
+          <TouchableOpacity
+            onPress={() => shuffle(numbers)}
+            style={{
+              width: Dimensions.get('window').width / 2 - 40,
+              alignItems: 'center',
+              justifyContent: 'center',
+              marginTop: 40,
+              height: 50,
+              borderRadius: 10,
+              backgroundColor: '#27A1FF',
+              marginBottom: 60,
+            }}>
+            <Text style={{color: '#fff', fontSize: 17, fontWeight: 'bold'}}>
+              랜덤 셔플
+            </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={() => setStatus(prev => !prev)}
+            style={{
+              width: Dimensions.get('window').width / 2 - 40,
+              alignItems: 'center',
+              justifyContent: 'center',
+              marginTop: 40,
+              height: 50,
+              borderRadius: 10,
+              backgroundColor: '#fff',
+              marginBottom: 60,
+              borderWidth: 2,
+              borderColor: '#27A1FF',
+            }}>
+            <Text style={{color: '#27A1FF', fontSize: 17, fontWeight: 'bold'}}>
+              결과 보기
+            </Text>
+          </TouchableOpacity>
+        </View>
+
+        <FlatList
+          data={data.type}
+          renderItem={renderRow}
+          numColumns={2}
+          keyExtractor={data => data.clean_name.toString()}
         />
-        <Text style={{fontSize: 24, fontWeight: 'bold'}}>청소 랜덤 뽑기</Text>
-      </View>
-
-      <View
-        style={{
-          flexDirection: 'row',
-          alignItems: 'center',
-          justifyContent: 'space-evenly',
-        }}>
-        <TouchableOpacity
-          onPress={() => shuffle(numbers)}
-          style={{
-            width: Dimensions.get('window').width / 2 - 40,
-            alignItems: 'center',
-            justifyContent: 'center',
-            marginTop: 40,
-            height: 50,
-            borderRadius: 10,
-            backgroundColor: '#27A1FF',
-            marginBottom: 60,
-          }}>
-          <Text style={{color: '#fff', fontSize: 17, fontWeight: 'bold'}}>
-            랜덤 셔플
-          </Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity
-          onPress={() => setStatus(prev => !prev)}
-          style={{
-            width: Dimensions.get('window').width / 2 - 40,
-            alignItems: 'center',
-            justifyContent: 'center',
-            marginTop: 40,
-            height: 50,
-            borderRadius: 10,
-            backgroundColor: '#fff',
-            marginBottom: 60,
-            borderWidth: 2,
-            borderColor: '#27A1FF',
-          }}>
-          <Text style={{color: '#27A1FF', fontSize: 17, fontWeight: 'bold'}}>
-            결과 보기
-          </Text>
-        </TouchableOpacity>
-      </View>
-
-      <FlatList
-        data={data.type}
-        renderItem={renderRow}
-        numColumns={2}
-        keyExtractor={data => data.clean_name.toString()}
-      />
+      </ScrollView>
     </>
   );
 };
